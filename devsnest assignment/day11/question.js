df63ecb1d6e73a13e940dc2f66d1fd0683226c13
ananyasingh4 <85430147+ananyasingh4@users.noreply.github.com>
2021-07-06 16:16:59 +0530

    doc: added day11 tha

---
+++ devsnest assignment/day11/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The 'I LOVE YOU' virus is an example of ?",
+    answer: "Script Virus",
+    options: [
+      "File infector virus",
+      "Script Virus",
+      "Resident Virus",
+      "Browser hijacker"
+    ]
+  },
+    {
+      numb: 2,
+      question: "Which is not a property of attribute Behaviour of Marquee tag?",
+      answer: "blur",
+      options: [
+          "alternate ",
+          "blur",
+          "scroll",
+          "slide"
+      ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: " Which of the following is not supported by older browsers? ",
+    answer: "All of the above",
+    options: [
+        "Layers",
+        "CSS",
+        "frames",
+        "All of the above"
+    ]
+},
+    {
+      numb: 5,
+      question: "Orkut.com is owned by ___________",
+      answer: "Google",
+      options: [
+          "Google",
+          "Yahoo ",
+          "Microsoft",
+          "Apple"
+    ]
+  },
+];
